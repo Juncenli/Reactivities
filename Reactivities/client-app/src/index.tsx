5b5fa9dc00ae3ff011b1f348cc9f8414e2900a9c
Juncenli <li.junce@northeastern.edu>
2023-06-23 13:26:06 -0400

    MobX

--- Reactivities/client-app/src/index.tsx
+++ Reactivities/client-app/src/index.tsx
@@ -1,17 +1,18 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './app/layout/styles.css';
 import 'semantic-ui-css/semantic.min.css'
+import './app/layout/styles.css';
 import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
+import { store, StoreContext } from './app/stores/store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  // 这个会导致call 两次Axios 因为渲染了两次
-  // <React.StrictMode>
+  <StoreContext.Provider value={store}>
     <App />
-  // </React.StrictMode>
+  </StoreContext.Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
