0a6f9c865ec72fc1983eae3830021547cf062fda
Juncenli <li.junce@northeastern.edu>
2023-06-26 11:00:30 -0400

    Styling the User Interface

--- Reactivities/client-app/src/features/activities/form/ActivityForm.tsx
+++ Reactivities/client-app/src/features/activities/form/ActivityForm.tsx
@@ -8,7 +8,7 @@ import {v4 as uuid} from 'uuid';
 
 export default observer(function ActivityForm() {
     const {activityStore} = useStore();
-    const {selectedActivity, createActivity, updateActivity, 
+    const {createActivity, updateActivity, 
         loading, loadActivity, loadingInitial} = activityStore;
     const {id} = useParams();
     const navigate = useNavigate();
@@ -46,12 +46,12 @@ export default observer(function ActivityForm() {
     return (
         <Segment clearing>
             <Form onSubmit={handleSubmit} autoComplete='off'>
-                <Form.Input placeholder='Title' value={activity.title} name='title' onChange={handleInputChange} />
-                <Form.TextArea placeholder='Description' value={activity.description} name='description' onChange={handleInputChange} />
-                <Form.Input placeholder='Category' value={activity.category} name='category' onChange={handleInputChange} />
-                <Form.Input type='date' placeholder='Date' value={activity.date} name='date' onChange={handleInputChange} />
-                <Form.Input placeholder='City' value={activity.city} name='city' onChange={handleInputChange} />
-                <Form.Input placeholder='Venue' value={activity.venue} name='venue' onChange={handleInputChange} />
+                <Form.Input required placeholder='Title' value={activity.title} name='title' onChange={handleInputChange} />
+                <Form.TextArea required placeholder='Description' value={activity.description} name='description' onChange={handleInputChange} />
+                <Form.Input required placeholder='Category' value={activity.category} name='category' onChange={handleInputChange} />
+                <Form.Input required type='date' placeholder='Date' value={activity.date} name='date' onChange={handleInputChange} />
+                <Form.Input required placeholder='City' value={activity.city} name='city' onChange={handleInputChange} />
+                <Form.Input required placeholder='Venue' value={activity.venue} name='venue' onChange={handleInputChange} />
                 <Button loading={loading} floated='right' positive type='submit' content='Submit' />
                 <Button as={Link} to='/activities' floated='right' type='button' content='Cancel' />
             </Form>
