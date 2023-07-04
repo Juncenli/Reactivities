78afd315954271f508f152210266c45254cee2ab
Juncenli <li.junce@northeastern.edu>
2023-07-04 13:15:35 -0400

    form

--- Reactivities/client-app/src/index.tsx
+++ Reactivities/client-app/src/index.tsx
@@ -5,6 +5,7 @@ import reportWebVitals from './reportWebVitals';
 import { store, StoreContext } from './app/stores/store';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './app/router/Routes';
+import "react-datepicker/dist/react-datepicker.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
